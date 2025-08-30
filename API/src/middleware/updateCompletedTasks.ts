e8c288b17a1d6a1212f1b76f2eb8502bdc61eab3
Eureka <narike@gmail.com>
2025-08-30 17:56:51 +0300

    Also check food consumption when assignment is complete

--- API/src/middleware/updateCompletedTasks.ts
+++ API/src/middleware/updateCompletedTasks.ts
@@ -5,11 +5,14 @@ import { Settler } from "../models/Player/Settler";
 import itemsCatalogue from "../data/itemsCatalogue.json";
 import { Inventory } from "../models/Player/Inventory";
 import { completeAssignmentsForColony } from "../services/assignmentService";
+import { processDailyFood } from "../services/processDailyFood";
 
 
 
 export const updateCompletedTasks =  async (req: Request, res: Response, next: NextFunction) => {
   const colony = req.colony;
   await completeAssignmentsForColony(colony);
+  await processDailyFood(colony); // re process daily tasks as settler might have brought
+    //needed food or might have to eat.
   next();
 }
